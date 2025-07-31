ffd758a137a4089c8eab4eb40e9cfd176cf8c7b4
OscarMontes <20233tn073@utez.edu.mx>
2025-07-31 11:16:07 -0600

    Correcion de servicios

--- src/components/Cliente/ClienteServicios.jsx
+++ src/components/Cliente/ClienteServicios.jsx
@@ -256,10 +256,20 @@ const fetchServices = async () => {
   const handlePaymentError = (error) => {
     Swal.fire('Error', error.message || 'Falló el pago.', 'error');
   };
+  <style jsx>{`
+                .navbar-fixed {
+                    position: fixed;
+                    top: 0;
+                    left: 0;
+                    right: 0;
+                    z-index: 1001; /* Más alto que el navbar original */
+                    width: 100%;
+                }'
+  `}</style>
 
   return (
     <div className={styles.container}>
-      <div className="fixed-top">
+      <div className="navbar-fixed">
         <Navbar title="- Servicios" />
       </div>
 
